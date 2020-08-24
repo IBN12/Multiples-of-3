c65cd414e9189a238eca3413b244b313279a1935
IBE12 <ibenge@msn.com>
2020-08-24 10:19:13 -0700

    Changed some text in the above multi-line comment header.

--- Multiples of 3/Main.cpp
+++ Multiples of 3/Main.cpp
@@ -2,7 +2,8 @@
 Program: Main (main program)
 Description:This is the user-defined program.
 
-Problem: If we list all the natural numbers below 10 that
+Problem: Project Euler #1
+-> If we list all the natural numbers below 10 that
 are multiples of 3 and 5, we get 3, 5, 6, and 9. The sum
 of these multiples is 23. Find the sum of all the multiples
 of 3 or 5 below 1000.
